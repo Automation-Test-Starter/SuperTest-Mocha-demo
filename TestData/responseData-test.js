b73ab7d4a678b702a0b752caa1743a3a1a971939
naodeng <nao.deng@thoughtworks.com>
2023-11-07 16:41:56 +0800

    update multiEnv demo

---
+++ TestData/responseData-test.js
@@ -0,0 +1,15 @@
+// Test response data file for dev environment
+module.exports = {
+    getAPI: {
+        "userId": 1,
+        "id": 1,
+        "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+        "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+    },  // response data for GET API
+    postAPI:{
+        "title": "foo",
+        "body": "bar",
+        "userId": 1,
+        "id": 101
+    },  // response data for POST API
+};
